5abc7ddba39be809de20b3d0e8f360538b3cd2ad
Sergei Timokhin <sergei.timokhin@statecraft.tech>
2024-11-08 20:03:35 +0200

    fix

--- backend/tests/test_containers/test_api.py
+++ backend/tests/test_containers/test_api.py
@@ -12,6 +12,15 @@ async def test_empty_rows(client: AsyncClient) -> None:
 
 
 @pytest.mark.asyncio()
+async def test_post_one_large_data(client: AsyncClient) -> None:
+    large_data = {"big_data": "test"}
+    response = await client.post("/large_data/", json=large_data)
+    assert response.status_code == 201
+    # if we don't have hash service the hash field is empty
+    assert response.json() == ""
+
+
+@pytest.mark.asyncio()
 async def test_post_one_large_data_with_hash_service(
     client: AsyncClient,
     hash_service: HashService,  # noqa: ARG001
